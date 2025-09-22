52801530248682987f14b426536ca71831b53c90
fredd-a <fredda.nyarko@gmail.com>
2025-09-22 18:46:27 +0000

    trial run

---
+++ src/PlayerSelection.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class PlayerSelection {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        System.out.print("Enter player height (in meters): ");
+        float height = input.nextFloat();
+
+        System.out.print("Enter player weight (in pounds) : ");;
+        int weight = input.nextInt();
+
+        System.out.print("Enter the player's jersey number: ");
+        int Jersey_number = input.nextInt();
+
+
+    }
+}
+
